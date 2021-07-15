42ffa902a6bec5f4268898aa229130f9ef6c5c29
Davidsapo <davidsza1290@gmail.com>
2021-07-15 21:56:09 +0300

    Question and Student entities added

---
+++ src/main/java/entity/Question.java
@@ -0,0 +1,37 @@
+package entity;
+
+import java.util.Arrays;
+
+public class Question {
+
+    private final String question;
+    private final String[] answers;
+    private final int correctAnswer;
+
+    public Question(String question, int correctAnswer, String ...answers) {
+        this.question = question;
+        this.answers = answers;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String[] getAnswers() {
+        return answers;
+    }
+
+    public int getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "question='" + question + '\'' +
+                ", answers=" + Arrays.toString(answers) +
+                ", correctAnswer=" + correctAnswer +
+                '}';
+    }
+}
---
+++ src/main/java/entity/Student.java
@@ -0,0 +1,18 @@
+package entity;
+
+public class Student {
+
+    private String name;
+    private String surname;
+    public int score;
+
+    public Student(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + surname + ": " + score;
+    }
+}
